03e44a0ed0ef4de7709dde6e8b28d613e7003d1b
Dmitrij Kosmakov <digital.artificial@gmail.com>
2020-07-01 02:54:41 +0300

    Add Rectangle Extensions tests

---
+++ Tests/src/Test_Rectangle_Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_DmitrijKosmakov_Danske;
+
+namespace Assignment_DmitrijKosmakov_Danske_Test
+{
+    [TestClass]
+    public class Test_Rectangle_Extensions
+    {
+        [TestMethod]
+        public void test_TallOrFlat()
+        {
+            Assert.AreEqual("square", new Rectangle(0, 0, 0, 0).TallOrFlat());
+            Assert.AreEqual("square", new Rectangle(0, 0, 1, 1).TallOrFlat());
+            Assert.AreEqual("square", new Rectangle(0, 0, 7, 7).TallOrFlat());
+
+            Assert.AreEqual("tall", new Rectangle(0, 0, 0, 1).TallOrFlat());
+            Assert.AreEqual("tall", new Rectangle(0, 0, 1, 2).TallOrFlat());
+            Assert.AreEqual("tall", new Rectangle(0, 0, 8, 9).TallOrFlat());
+
+            Assert.AreEqual("flat", new Rectangle(0, 0, 1, 0).TallOrFlat());
+            Assert.AreEqual("flat", new Rectangle(0, 0, 2, 1).TallOrFlat());
+            Assert.AreEqual("flat", new Rectangle(0, 0, 9, 8).TallOrFlat());
+        }
+    }
+}
